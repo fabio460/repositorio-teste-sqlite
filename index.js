93885e729a02e6009ecca0656659a558a59642ed
fabio <dasilvadeoliveirafabio@gmail.com>
2021-12-04 14:42:51 -0300

    teste 4

--- index.js
+++ index.js
@@ -2,40 +2,27 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize({
-  dialect:'sqlite',
-  storage:'./bancoDeDados.db'
-})
-
-  sequelize.authenticate().then(function(){
-      console.log('conectado com sucesso')
-  }).catch(function(erro){
-      console.log('falha ao se conectar: '+erro)
-  })
+//const sequelize = new Sequelize({
+  //dialect:'sqlite',
+  //storage:'./bancoDeDados.db'
+//})
 
 
-const modelPessoa = sequelize.define('pessoa',{
-  nome:{type:Sequelize.STRING},
-  idade:{type:Sequelize.STRING}
-})
+
+
+//const modelPessoa = sequelize.define('pessoa',{
+ // nome:{type:Sequelize.STRING},
+ // idade:{type:Sequelize.STRING}
+//})
 //modelPessoa.sync({force:true})
-const inserirPessoa = ()=>{
-  modelPessoa.create({
-    nome:"fabio",
-    idade:"39 anos"
-  })
-}
-if(inserirPessoa){
-  console.log('pessoa criada com sucesso')
-}else{
-  console.log('falha ao criar')
-}
 
-inserirPessoa()
+
+
+
 app.get('/',async (req,res)=>{
-  const pessoa = await modelPessoa.findAll()
-  console.log(pessoa)
-    res.json(pessoa)
+  //const pessoa = await modelPessoa.findAll()
+  //console.log(pessoa)
+    res.json('pessoa')
 })
 
 require('dotenv').config()
